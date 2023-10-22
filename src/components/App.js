cb844b209865fa3790ce1838dae1d35eecdcf770
Shivam Kumar gupta <gptshivam595@gmail.com>
2023-10-22 16:38:33 +0530

    js left

--- src/components/App.js
+++ src/components/App.js
@@ -11,32 +11,36 @@ class App extends React.Component{
         className="Header"
         >  
         
-        <a class="two" href="#home">Home</a>
-        <a class="three" href="#news">News</a>
-        <a class="four" href="#contact">Contact</a>
-        <a class="five" href="#about">About</a>
+          <a class="two" href="#home">Home</a>
+          <a class="three" href="#news">News</a>
+          <a class="four" href="#contact">Contact</a>
+          <a class="five" href="#about">About</a>
 
-        <a class="one" href="default.asp" target="_blank">HELP</a>
+          <a class="one" href="default.asp" target="_blank">HELP</a>
         </Header>
 
         <body>
         
-        <h2 id="split" data-text="SPLITBILL">SPLITBILL</h2>
-        
         </body>
-        <h5>Search: <input type="text" id="ip1" name="search"></input><br></br></h5>
-        first name:<input type="text" id="ip2" name="firstname"></input><br></br>
-        second name:<input type="text" id="ip3" name="secondname"></input>
-        
+          <h2 id="split" data-text="SPLITBILL">SPLITBILL</h2>
+          <h5>Search: <input type="text" id="ip1" name="search"></input><br></br></h5>
+          <h6>
+            <font color="white">
+            first name:<input type="text" id="ip2" name="firstname"></input><br></br>
+            second name:<input type="text" id="ip3" name="secondname"></input>
+            </font>
+          </h6>
+        <div class ="container">
+        </div>
         <table>
-        <tr>
-          <th>heading</th>
-          <th>heading2</th>
-        </tr>
-        <tr>
-          <td>row</td>
-          <td>row2</td>
-        </tr>
+          <tr>
+            <th>heading</th>
+            <th>heading2</th>
+          </tr>
+          <tr>
+            <td>row</td>
+            <td>row2</td>
+          </tr>
         </table>
         </div>
         
